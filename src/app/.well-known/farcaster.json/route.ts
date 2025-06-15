b6fbe7aa1f5be0ca28cbc10b7f6d48d70c41b2d0
Harlihm <107759287+Harlihm@users.noreply.github.com>
2025-06-15 16:37:05 +0100

    manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,20 +1,20 @@
 export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL;
+  const appUrl = "https://mini-app-template-ebon.vercel.app";
 
   const config = {
     accountAssociation: {
       header:
         "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
+      payload: "eyJkb21haW4iOiJtaW5pLWFwcC10ZW1wbGF0ZS1lYm9uLnZlcmNlbC5hcHAifQ",
       signature:
         "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
     },
     frame: {
       version: "1",
-      name: "Frames v2 Demo",
+      name: "Mini App Template",
       iconUrl: `${appUrl}/icon.png`,
       homeUrl: appUrl,
-      imageUrl: `${appUrl}/frames/hello/opengraph-image`,
+      imageUrl: `${appUrl}/opengraph-image`,
       buttonTitle: "Launch Frame",
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#f7f7f7",
